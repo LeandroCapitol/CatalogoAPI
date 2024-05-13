9b6812b3c0fff579da711961b2b68b6fef883b07
Leandro Capitol <leandrorezendecapitol@gmail.com>
2024-05-13 16:50:12 -0300

    ajustes jwt

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[contoller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _reoleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager,
+                             RoleManager<IdentityRole> reoleManager, IConfiguration configuration)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _reoleManager = reoleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user is not null && await _userManager.CheckPasswordAsync(user, model.Password!))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName!),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles) 
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+            }
+        }
+
+    }
+}
